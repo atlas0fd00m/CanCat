a8179b3aa4f94f4d55674b6f4d734d827ffdbe23
Tim Brom <thb@grimm-co.com>
2016-03-21 23:10:02 -0400

    Updates for more consistent functionality

--- unlock.py
+++ unlock.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 import cancat
+import time
 
 f = cancat.FordInterface(port="/dev/tty.usbmodem1421")
 
-f.CANxmit(58, "\x81\x4f\x00\x02\x00\x00\x00\x00")
+time.sleep(1)
+
+for i in range(1,10):
+    f.CANxmit(58, "\x81\x4f\x00\x02\x00\x00\x00\x00")
 
