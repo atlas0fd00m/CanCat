30b64b7b170319115836a31dda054818b3a413d2
atlas 0f d00m <atlas@r4780y.com>
2022-01-26 17:02:46 -0500

    vstruct embedding.

--- cancat/vstruct/__init__.py
+++ cancat/vstruct/__init__.py
@@ -564,7 +564,7 @@ def resolvepath(impmod, pathstr):
     return resolve(impmod, nameparts)
 
 # NOTE: Gotta import this *after* VStruct/VSArray defined
-from . import vstruct.defs as vs_defs
+from .vstruct import defs as vs_defs
 
 def getStructure(sname):
     """
